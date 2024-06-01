4fad56667050ec1ebbe9abd0f045d4ffe74730ab
spielbrecher <jagello@list.ru>
2024-06-02 00:25:53 +0900

    CustomTKinter interface added

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import customtkinter as tk
+import customtkinter as сtk
+import tkinter as tk
+
 from frame import QuestionAnswerApp
 
 if __name__ == '__main__':
-    root = tk.CTk()
+    root = tk.Tk()
     app = QuestionAnswerApp(root)
+    root.title('Анализ отчетов')
     root.geometry("640x480")
+    root.update()
     root.mainloop()
